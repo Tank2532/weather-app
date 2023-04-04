53a036821e22398de40638f4fe47083008b582a8
Jackson Smith <jacksonsmith@Jacksons-MacBook-Air.local>
2023-04-03 21:50:50 -0500

    DayJS

---
+++ script.js
@@ -0,0 +1,39 @@
+var currentCity = document.querySelector('#city');
+var currentDate = document.querySelector('#date');
+var currentTemp = document.querySelector('#temp');
+var currentPic = document.querySelector('#picture');
+var currentWind = document.querySelector('#wind');
+var currentHumid = document.querySelector('#humid');
+var dateOne = document.querySelector('#date-1');
+var picOne = document.querySelector('#picture-1');
+var tempOne = document.querySelector('#temp-1');
+var windOne = document.querySelector('#wind-1');
+var humidOne = document.querySelector('#humid-1');
+var dateTwo = document.querySelector('#date-2');
+var picTwo = document.querySelector('#picture-2');
+var tempTwo = document.querySelector('#temp-2');
+var windTwo = document.querySelector('#wind-2');
+var humidTwo = document.querySelector('#humid-2');
+var dateThree = document.querySelector('#date-3');
+var picThree = document.querySelector('#picture-3');
+var tempThree = document.querySelector('#temp-3');
+var windThree = document.querySelector('#wind-3');
+var humidThree = document.querySelector('#humid-3');
+var dateFour = document.querySelector('#date-4');
+var picFour = document.querySelector('#picture-4');
+var tempFour = document.querySelector('#temp-4');
+var windFour = document.querySelector('#wind-4');
+var humidFour = document.querySelector('#humid-4');
+var dateFive = document.querySelector('#date-5');
+var picFive = document.querySelector('#picture-5');
+var tempFive = document.querySelector('#temp-5');
+var windFive = document.querySelector('#wind-5');
+var humidFive = document.querySelector('#humid-5');
+var searchButton = document.querySelector('.search-btn');
+var searchInput = document.querySelector('.search-bar');
+
+var today = dayjs().format('MM/DD/YYYY');
+$(currentDate).text(today);
+
+
+

2b9accea68ff3b76dbb01356b35774213ffc44a7
Jackson Smith <jacksonsmith@Jacksons-MacBook-Air.local>
2023-04-06 12:57:03 -0500

    getLatLon

--- script.js
+++ script.js
@@ -31,9 +31,53 @@ var windFive = document.querySelector('#wind-5');
 var humidFive = document.querySelector('#humid-5');
 var searchButton = document.querySelector('.search-btn');
 var searchInput = document.querySelector('.search-bar');
+// Find way to store Lon and Lat from requestLonLat pull to place in here
+// var requestForecast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=265ae15a1f0d3a3b015bd08a0b58dba7";
+// Replace London with searchInput
+var requestLonLat = "http://api.openweathermap.org/geo/1.0/direct?q=" + 'London' + "&limit=5&appid=e555e88ca3b04d74c7f45d18751b7ab0"; 
+var lon = ''
+var lat = ''
 
 var today = dayjs().format('MM/DD/YYYY');
 $(currentDate).text(today);
 
+var nextDay = dayjs().add(1, 'day').format('MM/DD/YYYY');
+$(dateOne).text(nextDay);
 
+var oneDayAfter = dayjs().add(2, 'day').format('MM/DD/YYYY');
+$(dateTwo).text(oneDayAfter);
+
+var twoDayAfter = dayjs().add(3, 'day').format('MM/DD/YYYY');
+$(dateThree).text(twoDayAfter);
+
+var threeDayAfter = dayjs().add(4, 'day').format('MM/DD/YYYY');
+$(dateFour).text(threeDayAfter);
+
+var fourDayAfter = dayjs().add(5, 'day').format('MM/DD/YYYY');
+$(dateFive).text(fourDayAfter);
+
+async function getLonLat() {
+    const response = await fetch(requestLonLat);
+    const data = await response.json();
+    lat = data[0].lat;
+    lon = data[0].lon;
+}
+
+getForecast();
+
+// Doesn't list lon or lat in requestForecast url
+async function getForecast() {
+    alert(lon);
+    alert(lat);
+    var requestForecast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=265ae15a1f0d3a3b015bd08a0b58dba7";
+    const response2 = await fetch(requestForecast);
+    const data2 = await response2.json();
+    console.log(data2);
+}
+
+
+
+// searchButton.addEventListener('click', function (event) {
+    
+// });
 
